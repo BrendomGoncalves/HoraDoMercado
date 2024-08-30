9048470a5f5a2d8938bef0a2288b4283805e9254
BrendomGoncalves <brendom.vi@gmail.com>
2024-08-30 03:53:58 -0300

    feat: Adicionando observable

--- src/app/Services/produto.service.ts
+++ src/app/Services/produto.service.ts
@@ -1,15 +1,32 @@
 import {Injectable} from '@angular/core';
 import {AppDB} from "../appdb";
 import {Produto} from "../Models/produto.model";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProdutoService {
-  constructor(private db: AppDB) {}
+  private produtosSubject = new BehaviorSubject<Produto[]>([]);
+  produtos$ = this.produtosSubject.asObservable();
+
+  constructor(private db: AppDB) {
+    this.carregarProdutos().then(() => {
+      console.log('Produtos carregados...')
+    }).catch((err) => {
+      console.error(err)
+    });
+  }
+
+  private async carregarProdutos() {
+    const produtos = await this.obterProdutos();
+    this.produtosSubject.next(produtos);
+  }
 
   async adicionarProduto(produto: Produto): Promise<number> {
-    return this.db.produtos.add(produto);
+    const id = await this.db.produtos.add(produto);
+    await this.carregarProdutos();
+    return id;
   }
 
   async obterProdutos(): Promise<Produto[]> {
@@ -21,15 +38,25 @@ export class ProdutoService {
   }
 
   async atualizarProduto(produto: Produto): Promise<number> {
-    return this.db.produtos.update(produto.id, produto);
+    const updated = await this.db.produtos.update(produto.id, {
+      nome: produto.nome,
+      descricao: produto.descricao,
+      unidadeMedida: produto.unidadeMedida,
+      quantidade: produto.quantidade,
+      mercado: produto.mercado,
+    });
+    await this.carregarProdutos();
+    return updated;
   }
 
   async removerProduto(id: number): Promise<void> {
-    return this.db.produtos.delete(id);
+    await this.db.produtos.delete(id);
+    await this.carregarProdutos();
   }
 
-  async removerProdutos(ids: number[]): Promise<void>{
-    return this.db.produtos.bulkDelete(ids);
+  async removerProdutos(ids: number[]): Promise<void> {
+    await this.db.produtos.bulkDelete(ids);
+    await this.carregarProdutos();
   }
 }
 
