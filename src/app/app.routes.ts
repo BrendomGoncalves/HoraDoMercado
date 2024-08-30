d9c63a45c2ba0fc5fe7cee44938eda691d32f785
BrendomGoncalves <brendom.vi@gmail.com>
2024-08-30 01:16:26 -0300

    feat: Estruturando projeto, Adicionando provider de animação, Definindo Rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {ProdutosComponent} from "./Pages/produtos/produtos.component";
+import {PaginaInicialComponent} from "./Pages/pagina-inicial/pagina-inicial.component";
+import {CarrinhoComponent} from "./Pages/carrinho/carrinho.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PaginaInicialComponent, pathMatch: 'full' },
+  { path: 'pagina-inicial', component: PaginaInicialComponent },
+  { path: 'produtos', component: ProdutosComponent },
+  { path: 'carrinho', component: CarrinhoComponent },
+  { path: '**', redirectTo: '' }
+];
