e272d3d723a50eaa58a60a3416583546b61e66bb
BrendomGoncalves <brendom.vi@gmail.com>
2024-09-14 17:18:20 -0300

    feat: Inicializando variaveis de controle do dashboard

--- src/app/Pages/pagina-inicial/pagina-inicial.component.ts
+++ src/app/Pages/pagina-inicial/pagina-inicial.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProdutoEstatisticaService} from "../../Services/produto-estatistica.service";
+import {RouterLink} from "@angular/router";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-pagina-inicial',
   standalone: true,
   imports: [
+    RouterLink,
+    NgForOf
   ],
   templateUrl: './pagina-inicial.component.html',
   styleUrl: './pagina-inicial.component.css'
 })
-export class PaginaInicialComponent {
+export class PaginaInicialComponent implements OnInit{
+  quantidadeProdutos: number = 0;
+  quantidadeProdutosMercado: [string, number][] = [];
+  produtosMaiorQuantidade: [string, number][] = [];
 
+  constructor(private produtoEstatistica: ProdutoEstatisticaService) { }
+
+  async ngOnInit(): Promise<void> {
+    this.quantidadeProdutos = await this.produtoEstatistica.quantidadeProdutos();
+    this.quantidadeProdutosMercado = await this.produtoEstatistica.quantidadeProdutosMercado();
+    this.produtosMaiorQuantidade = await this.produtoEstatistica.produtosMaiorQuantidade();
+  }
 }
